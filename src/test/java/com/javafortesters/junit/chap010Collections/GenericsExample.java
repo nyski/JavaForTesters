4564f3a2d34af1c1f11f01685ca42fe57747424d
SILVERMEDIA\radoslaw.wartalski <radoslaw.wartalski@silvermedia.pl>
2018-06-14 16:06:26 +0200

    exceptions

---
+++ src/test/java/com/javafortesters/junit/chap010Collections/GenericsExample.java
@@ -0,0 +1,5 @@
+package com.javafortesters.junit.chap010Collections;
+
+public class GenericsExample
+{
+}
---
+++ src/test/java/com/javafortesters/junit/chap011Exceptions/examples.java
@@ -0,0 +1,5 @@
+package com.javafortesters.junit.chap011Exceptions;
+
+public class examples
+{
+}
